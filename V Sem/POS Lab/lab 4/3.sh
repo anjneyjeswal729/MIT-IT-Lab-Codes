bc4e51254cbb6a5e4aab1e5592f3d4cde9118d8c
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2024-08-22 15:02:48 +0530

    Create 3.sh

---
+++ V Sem/POS Lab/lab 4/3.sh
@@ -0,0 +1,17 @@
+n=$#
+arr=("$@")
+for ((i=0;i<$n;i++))
+do
+	for ((j=0;j<n-i-1;j++))
+	do
+		if [[ ${arr[j]} > ${arr[$j+1]} ]]; then
+			temp=${arr[j]}
+			arr[j]=${arr[$j+1]}
+			arr[$j+1]=$temp
+		fi
+	done
+done
+
+for i in "${arr[@]}"; do
+	echo $i
+done
