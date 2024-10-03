7a9cb51e3cbedfdec0f2acf25985d47fc96b19d9
Sanganwar Sai Charan <122984649+itsmessc@users.noreply.github.com>
2024-10-03 16:19:32 +0530

    Update 1.c

--- V Sem/POS Lab/lab 10/1.c
+++ V Sem/POS Lab/lab 10/1.c
@@ -36,6 +36,27 @@ int check(int n)
     return 0;
 }
 
+int find_optimal(struct LL *pages[], int frame_count, int p, int current_index) {
+    int farthest = -1, index_to_replace = -1;
+
+    for (int i = 0; i < frame_count; i++) {
+        int j;
+        for (j = current_index; j < p; j++) {
+            if (pages[i]->p == j) {
+                if (j > farthest) {
+                    farthest = j;
+                    break;
+                }
+            }
+        }
+        if (j == p) {
+            return i;
+        }
+    }
+
+    return index_to_replace; 
+}
+
 int main(int argc, char const *argv[])
 {
     int f, p;
@@ -95,43 +116,50 @@ int main(int argc, char const *argv[])
     printf("Enter number of frames and pages: ");
     scanf("%d %d", &f, &p);
     printf("Optimal\n");
-    c = 0;
-    for (int i = 0; i < p; i++)
-    {
-        int pid;
-        printf("Enter Pno: ");
-        scanf("%d", &pid);
+    struct LL *pages[f]; 
+    for (int i = 0; i < f; i++) {
+        pages[i] = NULL;
+    }
 
-        int n = check(pid);
-        if (!n)
-        {
-            if (c < f)
-            {
-                printf("eh");
+    int page_numbers[p]; 
+    for (int i = 0; i < p; i++) {
+        printf("Enter Pno: ");
+        scanf("%d", &page_numbers[i]);
+    }
+
+    for (int i = 0; i < p; i++) {
+        int pid = page_numbers[i];
+        if (!check(pid)) { 
+            if (pf < f) { 
                 struct LL *node = (struct LL *)malloc(sizeof(struct LL));
                 node->p = pid;
                 node->next = head;
                 head = node;
-                c++;
-            }
-            else
-            {
-                struct LL *oldest = head;
-                while (oldest->next->next != NULL)
-                {
-                    oldest = oldest->next;
+                pages[pf] = node;
+                pf++;
+            } else {
+                int replace_index = find_optimal(pages, f, p, i);
+                if (replace_index != -1) {
+                    struct LL *node = (struct LL *)malloc(sizeof(struct LL));
+                    node->p = pid;
+                    node->next = head;
+                    head = node;
+
+                    pages[replace_index] = node; 
                 }
-                oldest->next->p = pid;
-                oldest->next->next = head;
-                head = oldest->next;
-                oldest->next = NULL;
             }
+            printll(); 
         }
-        else
-        {
-            pf++;
-        }
-        printll();
     }
+    printf("Page faults: %d\n", pf);
+
+    curr = head;
+    while (curr != NULL) {
+        struct LL *temp = curr;
+        curr = curr->next;
+        free(temp);
+    }
+    return 0;
+
     return 0;
 }
