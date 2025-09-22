2e51ac2d914b9c749ecbe8801c7fe0108e9cb704
anjneyjeswal729 <anjneyjeswal.pubg@gmail.com>
2025-09-22 12:39:27 +0530

    Update 1.py

--- V Sem/IS-Lab/Lab3/1.py
+++ V Sem/IS-Lab/Lab3/1.py
@@ -1,3 +1,7 @@
+'''Using RSA, encrypt the message "Asymmetric Encryption" with the public key (n,
+ e). Then decrypt the ciphertext with the private key (n, d) to verify the original
+ message.'''
+
 from sympy import mod_inverse
 
 # RSA parameters (using larger primes for practicality)
