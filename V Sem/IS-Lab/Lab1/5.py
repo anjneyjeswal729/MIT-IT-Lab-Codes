df7ad2129b32a475eb95b0bcb2801e32ccf44f28
anjneyjeswal729 <anjneyjeswal.pubg@gmail.com>
2025-09-22 12:29:26 +0530

    Update 5.py

--- V Sem/IS-Lab/Lab1/5.py
+++ V Sem/IS-Lab/Lab1/5.py
@@ -1,3 +1,9 @@
+'''John is reading a mystery book involving cryptography. In one part of the book, the
+ author gives a ciphertext "CIW" and two paragraphs later the author tells the reader that
+ this is a shift cipher and the plaintext is "yes". In the next chapter, the hero found a tablet
+ in a cave with "XVIEWYWI" engraved on it. John immediately found the actual meaning
+ of the ciphertext.  Identify the type of attack and plaintext. '''
+
 def findkey(encmsg,pt):
     key=ord(encmsg[0])-ord(pt[0])
     return key%26
