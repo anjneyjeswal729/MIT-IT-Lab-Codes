1001ebcbd261677910daef23af741ff97ff0a4f5
anjneyjeswal729 <anjneyjeswal.pubg@gmail.com>
2025-09-22 12:35:51 +0530

    Update 2.py

--- V Sem/IS-Lab/Lab2/2.py
+++ V Sem/IS-Lab/Lab2/2.py
@@ -1,3 +1,7 @@
+'''Encrypt the message "Sensitive Information" using AES-128 with the following
+ key: "0123456789ABCDEF0123456789ABCDEF". Then decrypt the ciphertext to
+ verify the original message.'''
+
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad,unpad
 import base64
@@ -15,3 +19,4 @@ cipher_decrypt = AES.new(key, AES.MODE_CBC, iv=cipher.iv)
 plaintext = unpad(cipher_decrypt.decrypt(ctext), AES.block_size)
 print("Encryptes message: ",base64.b64encode(ctext).decode('ascii'))
 print(plaintext)
+
